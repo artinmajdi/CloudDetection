a7281f30b284c551cffef51f9c5af661798a150a
artinmajdi <msm2024@gmail.com>
2019-02-07 19:50:53 -0700

    s

--- main.m
+++ main.m
@@ -1,9 +1,11 @@
-clear
-clc
-close all
+% clear
+% clc
+% close all
 
 
 Dir = '/media/data1/artin/CloudData/images/';
-load('Pixels_geographical information.mat')
+imwrite(Land_MASK, [Dir , 'LandMask_from_LatLong.jpg'])
+ 
+% load('Pixels_geographical information.mat')
 
-extractObjects(Dir,area)
+% extractObjects(Dir,area)
